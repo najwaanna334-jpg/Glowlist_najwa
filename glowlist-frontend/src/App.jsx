e68ae52c2834e63353d4726fa6d762f88573730f
najwaanna334-jpg <najwaanna334@gmail.com>
2026-08-11 14:55:52 +0700

    ini glowlist produk dan kategori

---
+++ glowlist-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layaout";
+import Home from "./pages/Home";
+import Produk from "./pages/Produk";
+import Kategori from "./pages/Kategori";
+
+export default function App(){
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+      <Route index element={<Home/>}/>
+      <Route path="produk" element={<Produk/>}/>
+      <Route path="kategori" element={<Kategori/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
